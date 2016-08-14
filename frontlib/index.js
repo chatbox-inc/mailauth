723e6096b50835d4b76e3ffc321a2eae47bb1164
mikakane <mikakane2@gmail.com>
2016-08-14 23:12:54 +0900

    udpate

--- frontlib/index.js
+++ frontlib/index.js
@@ -2,18 +2,25 @@
 
 const Request = require("./src/request.js")
 const Register = require("./src/register.js")
+const Login = require("./src/login.js")
 
 
 class MailAuth{
 
-    constructor({url}){
+    constructor({url,store}){
+
         const request = new Request(url);
+
+        this.store = store;
         this.register = new Register({request});
+        this.login = new Login({request,store});
     }
 
     parse(){
         return {
-            register: this.register
+            register: this.register,
+            login:    this.login,
+            store:    this.store
         }
     }
 }
--- frontlib/spec/login/register.spec.js
+++ frontlib/spec/login/register.spec.js
@@ -2,31 +2,57 @@
 
 var assert = require("power-assert");
 
-module.exports = ({register})=>{
-    return ()=>{
+var createSampleUser = ()=>{
+    return {
 
-        var email = "t.goto+hogehoge@chatbox-inc.com";
-        var data = {}
-        var addData = {
-            password: "chatbox5678",
-            name: "後藤"
-        }
+    }
+}
+
+
+module.exports = ({store,register,login,profile})=>{
+
+    let userData = {
+        email: "t.goto+"+Math.random().toString(36).slice(-8)+"@chatbox-inc.com",
+        password: "chatbox5678",
+        name: "後藤"
+    }
+    let {email,password,name} = userData;
+
+    describe("REGISTER", ()=>{
 
         var mailToken = null
 
-        describe("SEE API",() => {
-            it('CAN REGISTER',() => {
-                return register.send(email,data).then(({token})=>{
-                    assert(token.key)
-                    mailToken = token.key
-                })
-            });
-            it('CAN CHECK TOKEN',() => {
-                return register.check(mailToken)
-            });
-            it('CAN REGISTER',() => {
-                return register.handle(mailToken,addData)
-            });
-        })
-    }
+        it('CAN REGISTER',() => {
+            return register.send(email,{}).then(({token})=>{
+                assert(token.key)
+                mailToken = token.key
+            })
+        });
+        it('CAN CHECK TOKEN',() => {
+            return register.check(mailToken)
+        });
+        it('CAN REGISTER',() => {
+            return register.handle(mailToken,{name,password})
+        });
+    })
+
+    describe("LOGIN", ()=>{
+
+        it('CAN LOGIN',() => {
+            return login.login({email,password}).then(({token})=>{
+                assert(token.key)
+                assert(store.get("LOGIN-TOKEN"))
+            })
+        });
+    })
+
+    describe("PROFILE", ()=>{
+
+        it('CAN GET',() => {
+            return login.login({email,password}).then(({token})=>{
+                assert(token.key)
+                assert(store.get("LOGIN-TOKEN"))
+            })
+        });
+    })
 }
